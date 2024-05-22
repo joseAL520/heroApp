aa99e3a06d72080d05528eca2ee043567d54f8d9
jose angel <jose.albelt1817@gmail.com>
2024-05-22 09:13:24 -0500

    ruta padre y rutas hijas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
-const routes: Routes = [];
+// rutas principales o ruta padre
+const routes: Routes = [
+
+  {
+    path:'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+  },
+  {
+    path:'heroes',
+    loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule),
+  },
+  {
+    // por si no se encuentra la direccion
+    path:'404',
+    component: Error404PageComponent
+  },
+  {
+    path:'',
+    redirectTo:'heroes',
+    pathMatch:'full' // ayuda para que no se redirecciones
+  },
+  {
+    path:'**',
+    redirectTo:'404',
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
