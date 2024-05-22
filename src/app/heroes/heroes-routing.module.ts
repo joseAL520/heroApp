173786a6ff27a2162d021e84d2ed4dcf512a39fe
jose angel <jose.albelt1817@gmail.com>
2024-05-22 09:25:16 -0500

    rutas nietas

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { AddNewHeroPageComponent } from './pages/add-new-hero-page/add-new-hero-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 
 // ruta hija
 const routes: Routes = [
   {
     path:'',
     component: LayoutPageComponent,
+
+    // rutas nietas
+    children:[
+      {path:'new-hero', component:AddNewHeroPageComponent},
+      {path:'search', component:SearchPageComponent},
+      {path:'edit/:id', component:AddNewHeroPageComponent},
+      {path:'list', component:ListPageComponent},
+      // esto es un comodin el :id simpre dejalo al final
+      {path:':id', component:HeroPageComponent},
+      {path:':**', redirectTo:'list'},
+      
+      
+
+    ]
     
   }
 ];
