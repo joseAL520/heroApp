60af0f596460de9f882d539338cae47b4633ea45
jose angel <jose.albelt1817@gmail.com>
2024-05-22 08:54:11 -0500

    creacion de componentes para hero

---
+++ src/app/heroes/pages/add-new-hero-page/add-new-hero-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-new-hero-page',
+  templateUrl: './add-new-hero-page.component.html',
+  styles: ``
+})
+export class AddNewHeroPageComponent {
+
+}
