cdca4764d97be2e31ccc03bc3145a69d73ba1ae9
jose angel <jose.albelt1817@gmail.com>
2024-05-24 08:49:13 -0500

    Mostrar Heroes eb listado

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -8,6 +8,7 @@ import { ListPageComponent } from './pages/list-page/list-page.component';
 import { AddNewHeroPageComponent } from './pages/add-new-hero-page/add-new-hero-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { MaterialModule } from '../material/material.module';
+import { CardComponent } from './components/card/card.component';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { MaterialModule } from '../material/material.module';
     LayoutPageComponent,
     ListPageComponent,
     AddNewHeroPageComponent,
-    SearchPageComponent
+    SearchPageComponent,
+    CardComponent
   ],
   imports: [
     CommonModule,
