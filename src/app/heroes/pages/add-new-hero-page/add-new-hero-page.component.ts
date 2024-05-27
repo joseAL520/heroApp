e979b43e59cd35dcbe8f1f2fe2ef1d3ce6245b07
jose angel <jose.albelt1817@gmail.com>
2024-05-27 09:49:42 -0500

    form control y form gruop

--- src/app/heroes/pages/add-new-hero-page/add-new-hero-page.component.ts
+++ src/app/heroes/pages/add-new-hero-page/add-new-hero-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Publisher } from '../../interfaces/hero.interfaces';
 
 @Component({
   selector: 'app-add-new-hero-page',
@@ -7,11 +9,31 @@ import { Component } from '@angular/core';
 })
 export class AddNewHeroPageComponent {
 
+  public heroForm = new FormGroup({
+    id:        new FormControl<string>(''),
+    superhero: new FormControl<string>('',{nonNullable:true}),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img:   new FormControl(''),
+  });
 
   public publishers = [
     { id: 'DC Comics', desc: 'DC - Comics' },
     { id: 'Marvel Comics', desc: 'Marvel - Comics' },
   ];
+
+
+  constructor(){}
+
+  onSubmit():void{
+
+    console.log({
+      formIsValid: this.heroForm.valid,
+      value: this.heroForm.value
+    })
+  }
 
 
 }
