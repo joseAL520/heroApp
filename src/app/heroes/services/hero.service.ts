53127f2d6d3c0abd86afa9125c9d20c35501f8be
jose angel <jose.albelt1817@gmail.com>
2024-05-27 09:12:53 -0500

    enpoint crud

--- src/app/heroes/services/hero.service.ts
+++ src/app/heroes/services/hero.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { enviroments } from '../../../environments/environments';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interfaces';
 
 
@@ -33,5 +33,29 @@ export class HeroService {
         return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`);
 
     }
+ 
     
+    //CRUD
+    
+    // agregar
+    addHero( hero:Hero):Observable<Hero>{
+        return this.http.post<Hero>(`${this.baseUrl}/heroes/`,hero);
+    }
+
+    // actualizar el .patch solo actualiza una parte del registro
+    updateHero( hero:Hero):Observable<Hero>{
+        if(!hero.id) throw Error('hero id is requerid');
+        return this.http.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`,hero);
+    }
+
+    // Eleminiar
+    deletHero( id:string):Observable<boolean>{
+        return this.http.delete<Hero>(`${this.baseUrl}/heroes/${id}`)
+            .pipe(
+                catchError(err => of (false)),
+                map( resp => true)
+            )
+    }
+
+
 }
