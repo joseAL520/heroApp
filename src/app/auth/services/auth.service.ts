3563b85bcd6d178433b3e9afef3032c1c8a4776c
jose angel <jose.albelt1817@gmail.com>
2024-05-28 10:28:50 -0500

    mantener la sesion del usuario y implementando el guards de angular

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,10 @@
 
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
 import { enviroments } from '../../../environments/environments';
 import { User } from '../interfaces/user.interface';
-import { Observable, tap } from 'rxjs';
+import { Observable, catchError, map, of, tap } from 'rxjs';
 
 
 @Injectable({providedIn: 'root'})
@@ -35,6 +36,21 @@ export class AuthService {
             tap( user => localStorage.setItem('token', 'aASDgjhasda.asdasd.aadsf123k' )),
           );
       }
+
+
+      // verifica si la secion esta activa
+    chekAuthentication():Observable<boolean> {
+      if ( !localStorage.getItem('token') ) return of(false);
+      const token = localStorage.getItem('token');
+      return this.http.get<User>(`${ this.baseUrl }/users/`)
+          .pipe(
+            tap( user => this.user = user ),
+            map( user => !!user ),
+            catchError( err => of(false) )
+      );
+    }
+
+
 
 
     logout(){
